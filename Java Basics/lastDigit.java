2fe22087ec72e2faf31b06ea1dc6a297d5e6e394
aditya-7562 <aditya.12114424@lpu.in>
2024-08-12 23:32:32 +0530

    did few questions on operators..

---
+++ Java Basics/lastDigit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class lastDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int m = Math.abs(n);
+
+        int ans = m%10;
+
+        System.out.println(ans);
+
+        sc.close();
+    }
+}
---
+++ Java Basics/sumofN.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class sumofN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = n*(n+1) / 2;
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
